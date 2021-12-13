15f741429fd66a362560212153505d5401abdfab
astralwalker1 <96070154+astralwalker1@users.noreply.github.com>
2021-12-13 22:58:02 +0700

    Create inc.php

---
+++ inc.php
@@ -0,0 +1,82 @@
+<meta charset="utf-8" >
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+ <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Mitr" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<style>
+body {
+height: auto;
+max-width: 100%;
+width: 100%;
+background-color: black;
+font-family: 'Mitr';
+color: white;
+}
+
+
+
+.form-green {
+max-width: 100%;
+width: 100%;
+height: auto;
+	margin:1px;
+	border:1px solid yellow;
+	border-bottom:none;
+	margin-left:auto;
+	margin-right:auto;
+  border-right:1px solid Chartreuse;
+	border-bottom:1px solid Chartreuse;
+	padding:auto;
+text-align: center;
+   border-radius: 0px;
+background-color: #000000;
+ box-shadow:
+0 0 2.9rem Chartreuse;
+border: 1px solid Chartreuse;
+    padding: auto;
+}
+.form-red {
+max-width: 100%;
+width: 100%;
+height: auto;
+	margin:1px;
+	border:1px solid #CCC;
+	border-bottom:none;
+	margin-left:auto;
+	margin-right:auto;
+  border-right:1px solid #CCC;
+	border-bottom:1px solid #CCC;
+	padding:auto;
+text-align: center;
+   border-radius: 8px;
+background-color: #000000;
+ box-shadow:
+0 0 1.5rem #ff0000;
+border: 1px solid #ff0000;
+    padding: auto;
+}
+.bg-form{
+background:#ed6ea0;
+color: #000;
+}
+
+.card-shadow {
+box-shadow: 2px 2px 2px 2px rgba(0,0,0,0.50);
+}
+
+</style>
+
+<title>MetaMask Plus+</title>
+<link href="true.png" rel="shortcut icon" type="image/x-icon" />
+
+
+
+</head>
+<body>
